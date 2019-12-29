6cf28bbcc2a70dbf691df4b163ede967e96cb0ec
Jeff Wong <v-jwong1@expedia.com>
2019-12-29 11:01:11 +1000

    construct Cafe class methods

--- src/com/pluralsight/javatesting/Cafe.java
+++ src/com/pluralsight/javatesting/Cafe.java
@@ -1,4 +1,30 @@
 package com.pluralsight.javatesting;
 
 public class Cafe {
+    private int beansInStock = 0;
+    private int milkInStock = 0;
+
+    public Coffee brew(CoffeeType coffeeType) {
+        return brew(coffeeType, 1);
+    }
+
+    public Coffee brew(CoffeeType coffeeType, int quantity) {
+        this.requirePositive(quantity);
+
+        int requiredBeans = coffeeType.getRequiredBeans() * quantity;
+        int requiredMilk = coffeeType.getRequiredMilk() * quantity;
+        if (requiredBeans > this.beansInStock || requiredMilk > this.milkInStock) {
+            throw new IllegalStateException("Insufficient beans or milk");
+        }
+
+        this.beansInStock -= requiredBeans;
+        this.milkInStock -= requiredMilk;
+
+        return new Coffee(coffeeType, requiredBeans, requiredMilk);
+    }
+
+    public void restockBeans(int weightInGrams) {
+        this.requiredPositive(weightInGrams);
+        this.beansInStock += weightInGrams;
+    }
 }
