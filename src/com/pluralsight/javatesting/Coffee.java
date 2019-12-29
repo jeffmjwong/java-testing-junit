0251301ff960eb81bab20dc5b59c481394ebd3c7
Jeff Wong <v-jwong1@expedia.com>
2019-12-29 10:50:45 +1000

    create CoffeeType enum

--- src/com/pluralsight/javatesting/Coffee.java
+++ src/com/pluralsight/javatesting/Coffee.java
@@ -1,5 +1,28 @@
 package com.pluralsight.javatesting;
 
 public final class Coffee {
+    private final CoffeeType coffeeType;
+    private final int beans;
+    private final int milk;
 
+    public Coffee(CoffeeType coffeeType, int beans, int milk) {
+        this.coffeeType = coffeeType;
+        this.beans = beans;
+        this.milk = milk;
+    }
+
+    public CoffeeType getCoffeeType() {
+        return this.coffeeType;
+    }
+    public int getBeans() {
+        return this.beans;
+    }
+    public int getMilk() {
+        return this.milk;
+    }
+
+    @Override
+    public String toString() {
+        return "Coffee{" + "type=" + this.coffeeType + ", beans=" + this.beans + ", milk=" + this.milk + "}";
+    }
 }
