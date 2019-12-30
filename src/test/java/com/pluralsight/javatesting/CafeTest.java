950cac6d9d047b486ab7d69fe7e915add90893f6
Jeff Wong <v-jwong1@expedia.com>
2019-12-30 14:07:49 +1000

    complete canBrewEspresso unit test

--- src/test/java/com/pluralsight/javatesting/CafeTest.java
+++ src/test/java/com/pluralsight/javatesting/CafeTest.java
@@ -1,10 +1,18 @@
 package com.pluralsight.javatesting;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CafeTest {
     @Test
     public void canBrewEspresso() {
-        assert(false);
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(CoffeeType.Espresso.getRequiredBeans());
+
+        Coffee coffee = cafe.brew(CoffeeType.Espresso);
+
+        Assert.assertEquals(CoffeeType.Espresso, coffee.getCoffeeType());
+        Assert.assertEquals(0, coffee.getMilk());
+        Assert.assertEquals(CoffeeType.Espresso.getRequiredBeans(), coffee.getBeans());
     }
 }
