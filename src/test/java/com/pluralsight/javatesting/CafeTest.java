a5336c7d96648d19092f305577e4e3725978ee54
Jeff Wong <v-jwong1@expedia.com>
2019-12-31 12:39:53 +1000

    create canBrewLatte test

--- src/test/java/com/pluralsight/javatesting/CafeTest.java
+++ src/test/java/com/pluralsight/javatesting/CafeTest.java
@@ -3,32 +3,50 @@ package com.pluralsight.javatesting;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static com.pluralsight.javatesting.CoffeeType.Espresso;
+import static com.pluralsight.javatesting.CoffeeType.Latte;
+
 public class CafeTest {
+    private static final int ESPRESSO_BEANS = Espresso.getRequiredBeans();
+    private static final int NO_MILK = 0;
+    private static final int NO_BEANS = 0;
+    private static final int LATTE_MILK = Latte.getRequiredMilk();
+
     @Test
     public void canBrewEspresso() {
         Cafe cafe = cafeWithBeans();
 
-        Coffee coffee = cafe.brew(CoffeeType.Espresso);
+        Coffee coffee = cafe.brew(Espresso);
 
-        Assert.assertEquals("Wrong coffee type", CoffeeType.Espresso, coffee.getCoffeeType());
-        Assert.assertEquals("Wrong amount of milk",0, coffee.getMilk());
-        Assert.assertEquals("Wrong number of beans", CoffeeType.Espresso.getRequiredBeans(), coffee.getBeans());
+        Assert.assertEquals("Wrong coffee type", Espresso, coffee.getCoffeeType());
+        Assert.assertEquals("Wrong amount of milk",NO_MILK, coffee.getMilk());
+        Assert.assertEquals("Wrong number of beans", ESPRESSO_BEANS, coffee.getBeans());
     }
 
     @Test
     public void brewingEspressoConsumesBeans() {
         Cafe cafe = cafeWithBeans();
 
-        Coffee coffee = cafe.brew(CoffeeType.Espresso);
+        Coffee coffee = cafe.brew(Espresso);
 
-        Assert.assertEquals(0, cafe.getBeansInStock());
+        Assert.assertEquals(NO_BEANS, cafe.getBeansInStock());
+    }
+
+    @Test
+    public void canBrewLatte() {
+        Cafe cafe = cafeWithBeans();
+        cafe.restockMilk(LATTE_MILK);
+
+        Coffee coffee = cafe.brew(Latte);
+
+        Assert.assertEquals("Wrong coffee type", Latte, coffee.getCoffeeType());
     }
 
     @Test(expected = IllegalStateException.class)
     public void lattesRequiresMilk() {
         Cafe cafe = cafeWithBeans();
 
-        Coffee coffee = cafe.brew(CoffeeType.Latte);
+        Coffee coffee = cafe.brew(Latte);
     }
 
     private Cafe cafeWithBeans() {
